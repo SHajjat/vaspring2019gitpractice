de1fc333be6d66e9f545a9c6decd1a2a146c609b
SHajjat <samir.hajjat@gmail.com>
2019-07-30 21:29:35 -0400

    changes to main class

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,5 +2,8 @@ public class AppTest {
 	public static void main(String[] args) {
 		System.out.println("hello world hello");
 		System.out.println("hello world hello");
+		System.out.println("hello world hello");
+		System.out.println("hello world hello");
+		System.out.println("hello world hello");
 	}
 }
