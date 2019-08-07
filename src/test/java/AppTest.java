820a23866fd75a28cc66803ded95620868e3d5e9
SHajjat <samir.hajjat@gmail.com>
2019-08-06 21:38:59 -0400

    any commit msg is fine

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -2,6 +2,8 @@ public class AppTest {
 	public static void main(String[] args) {
 		System.out.println("no idea whats going on");
 		System.out.println("Good morning");
+		System.out.println("Good morning");
+		System.out.println("Good morning");
 
 		
 	}
