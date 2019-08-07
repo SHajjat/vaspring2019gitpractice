536234949a51a510afcec896d8cc890ad16ead34
SHajjat <samir.hajjat@gmail.com>
2019-08-06 20:05:58 -0400

    pushing new code

--- src/test/java/LohinYrdyd.java
+++ src/test/java/LohinYrdyd.java
@@ -1,12 +1,12 @@
 public class LohinYrdyd {
 	public static void main(String[] args) {
-		System.out.println("Testing");
-		System.out.println("Testing");
-		System.out.println("Testing");
-		System.out.println("Testing");
-		System.out.println("Testing");
-		System.out.println("Testing");
-		System.out.println("Testing");
+		System.out.println("Testing 1");
+		System.out.println("Testing 2");
+		System.out.println("Testing 3");
+		System.out.println("Testing 4");
+		System.out.println("Testing 5");
+		System.out.println("Testing 6");
+	
 		
 
 	}
