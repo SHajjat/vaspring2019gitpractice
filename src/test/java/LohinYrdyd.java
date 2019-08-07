eb6189fa48e2d6c9829248e9c2f9de4889148fd6
SHajjat <50310830+SHajjat@users.noreply.github.com>
2019-08-06 20:17:38 -0400

    Update LohinYrdyd.java

--- src/test/java/LohinYrdyd.java
+++ src/test/java/LohinYrdyd.java
@@ -1,12 +1,7 @@
 public class LohinYrdyd {
 	public static void main(String[] args) {
 		System.out.println("Testing 1");
-		System.out.println("Testing 2");
-		System.out.println("Testing 3");
-		System.out.println("Testing 4");
-		System.out.println("Testing 5");
-		System.out.println("Testing 6");
-		System.out.println("Testing 7");
+		
 
 	}
 }
