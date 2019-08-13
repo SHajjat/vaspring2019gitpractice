08e7d1386d925d2c3ce6446c8242bcc05b68444d
SHajjat <samir.hajjat@gmail.com>
2019-08-13 19:42:01 -0400

    branch devolop

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,12 +1,10 @@
 public class AppTest {
 	public static void main(String[] args) {
-		System.out.println("no idea whats going on");
-		System.out.println("no idea whats going on");
-		System.out.println("no idea whats going on");
-		System.out.println("no idea whats going on");
-		System.out.println("Good morning");
-		System.out.println("Good morning");
-		System.out.println("Good morning");
+		System.out.println("line 211");
+		System.out.println("line 212");
+		System.out.println("line 213");
+		System.out.println("line 214");
+		
 
 		
 	}
