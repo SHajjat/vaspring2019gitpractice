21edba4252817893ed9dbf6d9d7ad96fba8abdf3
SHajjat <samir.hajjat@gmail.com>
2019-09-01 00:49:27 -0400

    update to class test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,17 +1,7 @@
+
+
 public class AppTest {
 	public static void main(String[] args) {
-		System.out.println("line 211");
-		System.out.println("line 212");
-		System.out.println("line 213");
-		System.out.println("line 214");
-		System.out.println("line 215");
-		System.out.println("line 216");
-		System.out.println("line 217");
-		System.out.println("line 218");
-		System.out.println("line 219");
-		System.out.println("line 220");
-		System.out.println("line 221");
-		System.out.println("this line is coming from master");
-
+	
 	}
 }
